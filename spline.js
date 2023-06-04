27cc8f32e856e29a39f0e9d365a83a43ad7a7af9
Susan Kiani <susankianim@gmail.com>
2023-06-04 12:09:51 +0330

    added f

--- spline.js
+++ spline.js
@@ -1,4 +1,5 @@
 import matrix from "matrix-js"
+import Polynomial from "polynomial"
 
 function solve(A, b) {
     A = matrix(A);
@@ -7,7 +8,32 @@ function solve(A, b) {
     return A_inv.prod(b)
 }
 
-let A = [[1, 1, -3 , 1], [-5, 3, -4, 1], [1, 0, 2, -1], [1,  2, 0, 0]]
-let b = [2*17, 0, 1*17, 12*17]
-console.log(solve(A, b));
+function rule(x) {return x**2}
+
+function f(x) {
+    if (x.length > 1) {
+        return (f(x.slice(1)) - f(x.slice(0, -1)))/(x[x.length - 1] - x[0])
+    }
+    else return rule(x)
+}
+
+let nodes = [1, 2, 5, 8, 9]
+let n = nodes.length
+let h = nodes.slice(1).map((num, i) => num - nodes[i]);
+h.unshift(NaN)
+let mu = h.slice(1).map((num, i) => h[i] / (num + h[i]));
+let lambda = mu.map(x => 1 - x);
+
+
+
+
+
+
+// let A = [[1, 1, -3 , 1], [-5, 3, -4, 1], [1, 0, 2, -1], [1,  2, 0, 0]]
+// let b = [2*17, 0, 1*17, 12*17]
+// console.log(solve(A, b));
+// console.log(new Polynomial("+1").pow(3).toString());
+
+
+
 
