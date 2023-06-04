d365f5b52cbe03b01efe5095e9f306bffaa40d63
Susan Kiani <susankianim@gmail.com>
2023-06-04 20:25:08 +0330

    spline is done

--- spline.js
+++ spline.js
@@ -8,24 +8,9 @@ function solve(A, b) {
     return A_inv.prod(b)
 }
 
-let rule = "x ** 2";
 let is_natural = true
-
-function f(x) {
-    if (x.length > 1) {
-        return (f(x.slice(1)) - f(x.slice(0, -1)))/(x[x.length - 1] - x[0]);
-    }
-    // else return eval(rule);
-    else {
-        if (x == 0) return 1
-        if (x == 1) return 4
-        if (x == 2) return 0
-        if (x == 3) return -2
-    }
-}
-
-let x_ = [0, 1, 2, 3];
-let values = x_.map(x => f(x))
+let rule = "x ** 2";
+let x_ = [-1, 0, 1, 2];
 
 let n = x_.length - 1;
 let h = x_.slice(1).map((num, i) => num - x_[i]);
@@ -44,18 +29,33 @@ if (is_natural) {
     A.map((row, i) => A[i] = row.slice(1, -1))
 }
 
+function f(x) {
+    if (x.length > 1) {
+        return (f(x.slice(1)) - f(x.slice(0, -1)))/(x[x.length - 1] - x[0]);
+    }
+    // else return eval(rule);
+    else {
+        if (x == -1) return 1
+        if (x == 0) return 0
+        if (x == 1) return -1
+        if (x == 2) return 4
+    }
+}
+
 let b = x_.slice(1, -1).map((num, i) => 6 * f([x_[i], num, x_[i + 2]]));
 
-
-let m = [0].concat(...solve(A, b)).concat([0]);
+let m = [];
+if (is_natural) {
+    m = [0].concat(...solve(A, b)).concat([0]);
+}
 
 let spline = []
 for (let i = 0; i < n; i++) {
-    let q = values[i] - h[i + 1] * m[i] / 6;
+    let q = f(x_[i]) - h[i + 1] * m[i] / 6;
     let p = f([x_[i], x_[i + 1]]) + h[i + 1] * (m[i] - m[i + 1]) / 6;
-    let polynomial_rule = new Polynomial(`x-${x_[i]}`).pow(3).mul(m[i + 1] / (6 * h[i + 1]))
+    let rule_i = new Polynomial("x").sub(x_[i]).pow(3).mul(m[i + 1] / (6 * h[i + 1]))
         .add(new Polynomial(`${x_[i + 1]}-x`).pow(3).mul(m[i] / (6 * h[i + 1])))
-        .add(new Polynomial(`x-${x_[i]}`).mul(p)).add(q);
-    spline[i] = polynomial_rule.toString();
+        .add(new Polynomial("x").sub(x_[i]).mul(p)).add(q);
+    spline[i] = rule_i.toString();
 }
 console.log(spline);
