4daebf16c36dfdd158f076464a4bb376fd36b315
Susan Kiani <susankianim@gmail.com>
2023-06-04 21:04:07 +0330

    added class

--- spline.js
+++ spline.js
@@ -5,57 +5,82 @@ function solve(A, b) {
     A = matrix(A);
     b = matrix(matrix(b).map(x => [x]));
     let A_inv = matrix(A.inv());
-    return A_inv.prod(b)
+    return A_inv.prod(b);
 }
 
-let is_natural = true
-let rule = "x ** 2";
-let x_ = [-1, 0, 1, 2];
-
-let n = x_.length - 1;
-let h = x_.slice(1).map((num, i) => num - x_[i]);
-h.unshift(NaN);
-let mu = h.slice(1).map((num, i) => h[i] / (num + h[i]));
-let lambda = mu.map(x => 1 - x);
-
-let A = Array.from(Array(n - 1), () => new Array(n + 1).fill(0))
-for (let i = 1; i < n; i++) {
-    A[i - 1][i - 1] = mu[i]
-    A[i - 1][i] = 2
-    A[i - 1][i + 1] = lambda[i]
-}
-
-if (is_natural) {
-    A.map((row, i) => A[i] = row.slice(1, -1))
-}
-
-function f(x) {
-    if (x.length > 1) {
-        return (f(x.slice(1)) - f(x.slice(0, -1)))/(x[x.length - 1] - x[0]);
+class Issue{
+    constructor(rule, x_, is_natural = true) {
+        this.is_natural = is_natural
+        this.rule = rule;
+        this.x_ = x_;
+        this.n =  x_.length - 1;
+        this.h = x_.slice(1).map((num, i) => num - x_[i]);
+        this.h.unshift(NaN);
     }
-    // else return eval(rule);
-    else {
-        if (x == -1) return 1
-        if (x == 0) return 0
-        if (x == 1) return -1
-        if (x == 2) return 4
+
+    f(x) {
+        if (x.length > 1) {
+            return (this.f(x.slice(1)) - this.f(x.slice(0, -1)))/(x[x.length - 1] - x[0]);
+        }
+        // else return eval(rule);
+        else {
+            if (x == -1) return 1
+            if (x == 0) return 0
+            if (x == 1) return -1
+            if (x == 2) return 4
+        }
+    }
+
+    make_matrix_A() {
+        let mu = this.h.slice(1).map((num, i) => this.h[i] / (num + this.h[i]));
+        let lambda = mu.map(x => 1 - x);
+        
+        let A = Array.from(Array(this.n - 1), () => new Array(this.n + 1).fill(0))
+        for (let i = 1; i < this.n; i++) {
+            A[i - 1][i - 1] = mu[i]
+            A[i - 1][i] = 2
+            A[i - 1][i + 1] = lambda[i]
+        }
+        
+        if (this.is_natural) {
+            A.map((row, i) => A[i] = row.slice(1, -1))
+        }
+        return A;
+    }
+
+    make_matrix_b() {
+        let b = this.x_.slice(1, -1).map((num, i) => 6 * this.f([this.x_[i], num, this.x_[i + 2]]));
+        return b
+    }
+
+    make_matrix_m() {
+        let m = [];
+        if (this.is_natural) {
+            m = [0].concat(...solve(this.make_matrix_A(), this.make_matrix_b())).concat([0]);
+        }
+        return m
+    }
+
+    make_spline() {
+        let spline = []
+        let m = this.make_matrix_m()
+        for (let i = 0; i < this.n; i++) {
+            let q = this.f(this.x_[i]) - this.h[i + 1] * m[i] / 6;
+            let p = this.f([this.x_[i], this.x_[i + 1]]) + this.h[i + 1] * (m[i] - m[i + 1]) / 6;
+            let rule_i = new Polynomial("x").sub(this.x_[i]).pow(3).mul(m[i + 1] / (6 * this.h[i + 1]))
+                .add(new Polynomial(`${this.x_[i + 1]}-x`).pow(3).mul(m[i] / (6 * this.h[i + 1])))
+                .add(new Polynomial("x").sub(this.x_[i]).mul(p)).add(q);
+            spline[i] = rule_i.toString();
+        }
+        // return spline
+        let obj = {}
+        this.x_.slice(1).forEach((node, i) => obj[[this.x_[i], node]] = spline[i])
+        return obj
     }
 }
 
-let b = x_.slice(1, -1).map((num, i) => 6 * f([x_[i], num, x_[i + 2]]));
 
-let m = [];
-if (is_natural) {
-    m = [0].concat(...solve(A, b)).concat([0]);
-}
 
-let spline = []
-for (let i = 0; i < n; i++) {
-    let q = f(x_[i]) - h[i + 1] * m[i] / 6;
-    let p = f([x_[i], x_[i + 1]]) + h[i + 1] * (m[i] - m[i + 1]) / 6;
-    let rule_i = new Polynomial("x").sub(x_[i]).pow(3).mul(m[i + 1] / (6 * h[i + 1]))
-        .add(new Polynomial(`${x_[i + 1]}-x`).pow(3).mul(m[i] / (6 * h[i + 1])))
-        .add(new Polynomial("x").sub(x_[i]).mul(p)).add(q);
-    spline[i] = rule_i.toString();
-}
-console.log(spline);
+
+let my_issue = new Issue("x + 1", [-1, 0, 1, 2])
+console.log(my_issue.make_spline());
