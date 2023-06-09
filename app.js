850eef637b44da890846a6d57df55cc3f20f3911
Susan Kiani <susankianim@gmail.com>
2023-06-09 15:18:40 +0330

    updated

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 import { Issue } from "./spline.js";
-import { convertSplineToHTML } from "./toHTML.js"
 import { Chart } from "chart.js/auto";
 
+function convertPolynomialToHTML(polynomial) {
+    // return polynomial.replaceAll(/(\^)(\d+)/g, "<sup><small>$2</small></sup>")
+    return `\\(${polynomial}\\)`;
+}
 
-function solve(form) {
+function convertLimitToHTML(limit) {
+    let start = limit[0];
+    let end = limit[1];
+    return `\\(${start} ≤ x ≤ ${end}\\)`;
+}
+
+function convertRuleToHTML(rule) {
+    let lHTML = convertLimitToHTML(rule[0])
+    let pHTML = convertPolynomialToHTML(rule[1])
+    return `${pHTML}&emsp;&emsp;${lHTML}`
+}
+
+function convertSplineToHTML(splineArr) {
+    let ruleArr = splineArr.map(rule => convertRuleToHTML(rule))
+    return ruleArr.join("<br>")
+}
+
+export { convertSplineToHTML}
+function solve({knownXs, functionRule, knownValues}) {
     try {
-        let chartStatus = Chart.getChart("myChart");
-        if (chartStatus != undefined) {
-            chartStatus.destroy();
-        }
-        let x_ = [...new Set((form.knownXs.value).match(/-?\d+\.?\d*/g).map(Number))];
-        let rule = form.functionRule.value;
+        console.log({knownXs, functionRule, knownValues});
+        let x_ = [...new Set((knownXs).match(/-?\d+\.?\d*/g).map(Number))];
+        let rule = functionRule;
         let myIssue;
         if(rule) {
             x_ = x_.sort((a, b) => a - b);
             myIssue = new Issue(x_, null, rule);
         } else {
-            let values = (form.knownValues.value).match(/-?\d+\.?\d*/g).map(Number)
+            let values = (knownValues).match(/-?\d+\.?\d*/g).map(Number)
             let points = x_.map((x, i) => [x, values[i]]);
             x_ = x_.sort((a, b) => a - b);
             values = points.sort((a, b) => a[0] - b[0]).map(x => x[1]);
@@ -25,24 +43,42 @@ function solve(form) {
         let mySpline = myIssue.make_spline();
         let mySplineStr = convertSplineToHTML(mySpline);
 
-        let splineContainer = document.getElementById('spline-container');
-        splineContainer.innerHTML = mySplineStr;
-
         let chartData = myIssue.getChartData();
-        let canvas = document.getElementById('myChart');
-        let myChart = new Chart(canvas, { type: 'line', data: chartData });
-        myChart.update();
+        
+        return {mySplineStr, chartData};
     } catch (e) {
         console.log(e);
     }
 
 }
 
+function update({mySplineStr, chartData}) {
+    try {
+        let chartStatus = Chart.getChart("myChart");
+        if (chartStatus) {
+            chartStatus.destroy();
+        }
+        let splineContainer = document.getElementById("spline-container");
+        splineContainer.innerHTML = mySplineStr;
+
+        let canvas = document.getElementById("myChart");
+        let myChart = new Chart(canvas, { type: "line", data: chartData });
+        myChart.update();
+        MathJax.typeset();
+    } catch(e) {
+        console.log(e);
+    }
+}
+
 window.addEventListener("load", event => {
-    document.querySelector("#btnSubmit").addEventListener("click", (event) => {
+    let form = document.querySelector("#frmMain");
+    form.addEventListener("submit", (event) => {
         try {
-            let form = document.querySelector("#frmMain");
-            solve(form);
+            let knownXs = form.knownXs.value;
+            let functionRule = form.functionRule.value;
+            let knownValues = form.knownValues.value;
+            let {mySplineStr, chartData} = solve({knownXs, functionRule, knownValues});
+            update({mySplineStr, chartData})
         } catch (e) {
             console.log(e);
         }
--- docs/app.js
+++ docs/app.js
@@ -5775,28 +5775,6 @@
   var ms = new Issue([-1, 0, 1, 2], [1, 0, 1, 4]);
   console.log(ms.make_spline());
 
-  // toHTML.js
-  init_dirname();
-  init_buffer2();
-  init_process2();
-  function convertPolynomialToHTML(polynomial) {
-    return polynomial.replaceAll(/(\^)(\d+)/g, "<sup><small>$2</small></sup>");
-  }
-  function convertLimitToHTML(limit) {
-    let start = limit[0];
-    let end = limit[1];
-    return `${start} \u2264 x \u2264 ${end}`;
-  }
-  function convertRuleToHTML(rule) {
-    let lHTML = convertLimitToHTML(rule[0]);
-    let pHTML = convertPolynomialToHTML(rule[1]);
-    return `${pHTML}&emsp;&emsp;${lHTML}`;
-  }
-  function convertSplineToHTML(splineArr) {
-    let ruleArr = splineArr.map((rule) => convertRuleToHTML(rule));
-    return ruleArr.join("<br>");
-  }
-
   // node_modules/chart.js/auto/auto.js
   init_dirname();
   init_buffer2();
@@ -20062,20 +20040,34 @@
   Chart.register(...registerables);
 
   // app.js
-  function solve2(form) {
+  function convertPolynomialToHTML(polynomial) {
+    return `\\(${polynomial}\\)`;
+  }
+  function convertLimitToHTML(limit) {
+    let start = limit[0];
+    let end = limit[1];
+    return `\\(${start} \u2264 x \u2264 ${end}\\)`;
+  }
+  function convertRuleToHTML(rule) {
+    let lHTML = convertLimitToHTML(rule[0]);
+    let pHTML = convertPolynomialToHTML(rule[1]);
+    return `${pHTML}&emsp;&emsp;${lHTML}`;
+  }
+  function convertSplineToHTML(splineArr) {
+    let ruleArr = splineArr.map((rule) => convertRuleToHTML(rule));
+    return ruleArr.join("<br>");
+  }
+  function solve2({ knownXs, functionRule, knownValues }) {
     try {
-      let chartStatus = Chart.getChart("myChart");
-      if (chartStatus != void 0) {
-        chartStatus.destroy();
-      }
-      let x_ = [...new Set(form.knownXs.value.match(/-?\d+\.?\d*/g).map(Number))];
-      let rule = form.functionRule.value;
+      console.log({ knownXs, functionRule, knownValues });
+      let x_ = [...new Set(knownXs.match(/-?\d+\.?\d*/g).map(Number))];
+      let rule = functionRule;
       let myIssue;
       if (rule) {
         x_ = x_.sort((a, b) => a - b);
         myIssue = new Issue(x_, null, rule);
       } else {
-        let values = form.knownValues.value.match(/-?\d+\.?\d*/g).map(Number);
+        let values = knownValues.match(/-?\d+\.?\d*/g).map(Number);
         let points = x_.map((x2, i) => [x2, values[i]]);
         x_ = x_.sort((a, b) => a - b);
         values = points.sort((a, b) => a[0] - b[0]).map((x2) => x2[1]);
@@ -20083,21 +20075,37 @@
       }
       let mySpline = myIssue.make_spline();
       let mySplineStr = convertSplineToHTML(mySpline);
+      let chartData = myIssue.getChartData();
+      return { mySplineStr, chartData };
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  function update({ mySplineStr, chartData }) {
+    try {
+      let chartStatus = Chart.getChart("myChart");
+      if (chartStatus) {
+        chartStatus.destroy();
+      }
       let splineContainer = document.getElementById("spline-container");
       splineContainer.innerHTML = mySplineStr;
-      let chartData = myIssue.getChartData();
       let canvas = document.getElementById("myChart");
       let myChart = new Chart(canvas, { type: "line", data: chartData });
       myChart.update();
+      MathJax.typeset();
     } catch (e) {
       console.log(e);
     }
   }
   window.addEventListener("load", (event) => {
-    document.querySelector("#btnSubmit").addEventListener("click", (event2) => {
+    let form = document.querySelector("#frmMain");
+    form.addEventListener("submit", (event2) => {
       try {
-        let form = document.querySelector("#frmMain");
-        solve2(form);
+        let knownXs = form.knownXs.value;
+        let functionRule = form.functionRule.value;
+        let knownValues = form.knownValues.value;
+        let { mySplineStr, chartData } = solve2({ knownXs, functionRule, knownValues });
+        update({ mySplineStr, chartData });
       } catch (e) {
         console.log(e);
       }
